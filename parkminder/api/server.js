9f15a3927d3eb35e8a2319e2e92aef676bb981a1
Angela Montez <monteza@oregonstate.edu>
2022-10-09 20:26:24 -0700

    Node.js and Express set-up

---
+++ parkminder/api/server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app     = express()
+
+const cors    = require("cors")
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get("/", function(req, res) {
+    res.send("It's Working!")
+})
+
+app.listen(3000, () => {
+    console.log("app listening on port 3000")
+})
