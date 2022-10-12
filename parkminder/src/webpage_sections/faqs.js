83fc27419eb38267c6e067d3c6bb1f3253f55f2a
Angela Montez <monteza@oregonstate.edu>
2022-10-12 15:40:31 -0700

    Change faqs from cards to accordian

--- parkminder/src/webpage_sections/faqs.js
+++ parkminder/src/webpage_sections/faqs.js
@@ -1,24 +1,45 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
+import Accordion from 'react-bootstrap/Accordion'
 
 function FAQsPage() {
   return (
     <Container fluid id='faq'>
         <Row><h5>FAQs</h5></Row>
         <Row><h1>Resources</h1></Row>
-        <Row>
-            <Col>
-                <Card>
-                <Card.Body>
-                    <Card.Text>
-                    FILL ME IN
-                    </Card.Text>
-                </Card.Body>
-                </Card>
-            </Col>
-        </Row>
+
+        <Accordion flush>
+            <Accordion.Item eventKey="0">
+                <Accordion.Header>FILL ME IN</Accordion.Header>
+                <Accordion.Body>
+                FILL ME IN
+                </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="1">
+                <Accordion.Header>FILL ME IN</Accordion.Header>
+                <Accordion.Body>
+                FILL ME IN
+                </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="2">
+                <Accordion.Header>FILL ME IN</Accordion.Header>
+                <Accordion.Body>
+                FILL ME IN
+                </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="3">
+                <Accordion.Header>FILL ME IN</Accordion.Header>
+                <Accordion.Body>
+                FILL ME IN
+                </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="4">
+                <Accordion.Header>FILL ME IN</Accordion.Header>
+                <Accordion.Body>
+                FILL ME IN
+                </Accordion.Body>
+            </Accordion.Item>
+        </Accordion>
     </Container>
   );
 }
