465ddf46d773a429b4d60f73efc4c5861efb6800
Angela Montez <monteza@oregonstate.edu>
2022-10-16 17:00:43 -0700

    Add Navigation. Bug in display

--- parkminder/src/components/map.js
+++ parkminder/src/components/map.js
@@ -1,29 +1,48 @@
-import React, { useRef, useEffect, useState }from "react";
-import mapboxgl from "mapbox-gl";
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWxtb250ZXoiLCJhIjoiY2w5OG9sYmNrMDdzZjNwdXB2Ym9tNnlsaSJ9.7qMcqrkDozh6eKMTolbQdg'
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWxtb250ZXoiLCJhIjoiY2w5OG9sYmNrMDdzZjNwdXB2Ym9tNnlsaSJ9.7qMcqrkDozh6eKMTolbQdg';
 
 function Map() {
-    const mapContainer = useRef(null);
-    const map = useRef(null);
-    const [lat, setLat] = useState(37.753502532309014);
-    const [lng, setLng] = useState(-122.4378588051518);
-    const [zoom, setZoom] = useState(11.75);
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-122.4378588051518);
+  const [lat, setLat] = useState(37.753502532309014);
+  const [zoom, setZoom] = useState(11.5);
 
-    useEffect(()=>{
-        if (map.current) return;
-        map.current = new mapboxgl.Map({
-            container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [lng, lat],
-            zoom: zoom
-        });
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom
     });
+  });
 
-    return (
-        <div ref={mapContainer} className="map-container"></div>
-    )
+  useEffect(() => {
+    if (!map.current) return; // wait for map to initialize
+    map.current.on('move', () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+  });
 
+  useEffect(()=> {
+    if (!map.current) return;
+    map.current.addControl(new mapboxgl.NavigationControl(), 'top-right');
+  })
+
+  return (
+    <div>
+      <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div ref={mapContainer} className="map-container" />
+    </div>
+  );
 }
+
 
 export default Map;
