16769d00ef6e34100b8b2d99dd30370b72d861ca
Lauren Graham <lagraham337@gmail.com>
2022-10-29 14:01:04 -0700

    accommodate multiple user jobs

--- email_micros/mail_app.js
+++ email_micros/mail_app.js
@@ -1,34 +1,37 @@
 //user_data. object retrieval in JS in objectName.keyName or objectName["keyName"]
-const user_data = {
-    email: 'grahaml@oregonstate.edu',
-    date: '2022-10-28 23:17:59'
-}
+let users = [
+    {"name" : "mahesh", "email" : "grahaml@oregonstate.edu", "date" : '2022-10-29 13:58:59'},
+    {"name" : "suresh", "email" : "lagraham337@gmail.com", "date" : '2022-10-29 13:58:59'},
+    {"name" : "ramesh", "email" : "laurengrahamslp@gmail.com", "date" : '2022-10-29 13:58:59'}
+ ]
+
 
 //node-schedule
 const schedule = require('node-schedule');
-const date = new Date(user_data.date);
 
-//email job
-const job = schedule.scheduleJob(date, function(){
-    const sgMail = require('@sendgrid/mail')
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-    const msg = {
-    template_id: 'd-2a239b889605432e810f1ad55467f1ef',
-        to: user_data["email"], // Change to your recipient
-        from: 'ParkminderSF@gmail.com', // Change to your verified sender
-        subject: 'Sending with SendGrid is Fun!!!',
-        text: 'and easy to do anywhere, even with Node.js',
-        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-    }
-    sgMail
-        .send(msg)
-        .then(() => {
-            console.log('Email sent')
+users.forEach(async(user_data) => {
+    schedule.scheduleJob(user_data.date, async() => {
+        const sgMail = require('@sendgrid/mail')
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+        const msg = {
+            template_id: 'd-2a239b889605432e810f1ad55467f1ef',
+            to: user_data["email"], // Change to your recipient
+            from: 'ParkminderSF@gmail.com', // Change to your verified sender
+            subject: 'Sending with SendGrid is Fun!!!',
+            text: 'and easy to do anywhere, even with Node.js',
+            html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+        }
+        try { 
+            const something = await sgMail.send(msg)
+            .then(() => {
+                console.log('Email sent')
         })
-        .catch((error) => {
-            console.error(error)
-        })
-});
+        } catch(e) {
+           console.log(e)
+        }
+    })
+})
+
 
 // get date and time right now in JS, if needed:
 
