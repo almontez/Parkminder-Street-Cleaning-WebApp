9405844bc6bf4a7055a76805d8f85cf10dec6928
Lauren Graham <lagraham337@gmail.com>
2022-11-01 11:48:24 -0700

    added 400 error message if non JSON is presented

--- email_micros/index.js
+++ email_micros/index.js
@@ -11,27 +11,31 @@ app.use(express.json())
 app.post('/api/send/users', async function(req, res) {
     const users = req.body;
 
-    await users.forEach(async(user_data) => { // send multiple jobs
-      schedule.scheduleJob(user_data.date, async() => {
-        const sgMail = require('@sendgrid/mail')
-        sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-        const msg = {
-            template_id: 'd-2a239b889605432e810f1ad55467f1ef',
-            to: user_data["email"], // Change to your recipient
-            from: 'ParkminderSF@gmail.com', // Change to your verified sender
-            subject: 'Sending with SendGrid is Fun!!!',
-            text: 'and easy to do anywhere, even with Node.js',
-            html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-        }
-        try { 
-            await sgMail.send(msg)
-            console.log(`An email was sent to ${user_data["name"]}`)
-        } catch(e) {
-          console.log(e)
-        }
+    if (Array.isArray(users)) {
+      await users.forEach(async(user_data) => { // send multiple jobs
+        schedule.scheduleJob(user_data.date, async() => {
+          const sgMail = require('@sendgrid/mail')
+          sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+          const msg = {
+              template_id: 'd-2a239b889605432e810f1ad55467f1ef',
+              to: user_data["email"], // Change to your recipient
+              from: 'ParkminderSF@gmail.com', // Change to your verified sender
+              subject: 'Sending with SendGrid is Fun!!!',
+              text: 'and easy to do anywhere, even with Node.js',
+              html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+          }
+          try { 
+              await sgMail.send(msg)
+              console.log(`An email was sent to ${user_data["name"]}`)
+          } catch(e) {
+            console.log(e)
+          }
+        })
       })
-    })
-    res.end('Notifications scheduled.');
+      res.end('Notifications scheduled.');
+  } else {
+    res.status(400).end('Must be a JSON array of users.');
+  }
 });
 
 const server = app.listen(3000);
