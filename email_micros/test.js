e15685276349fa2823b7173578db2531aec3ff68
Angela Montez <angelali.montez@gmail.com>
2022-11-10 21:37:18 -0800

    Add .env file to parkminder

--- email_micros/test.js
+++ email_micros/test.js
@@ -2,7 +2,7 @@
 const request = require('request');
 const userData = require('./users.json');
 
-request.post({ url: 'http://localhost:3000/api/send/users', body: userData, json: true}, function(error, response, body){
+request.post({ url: 'http://localhost:3356/api/send/users', body: userData, json: true}, function(error, response, body){
     console.log(`${response.statusCode} - ${body}`);
 });
 
